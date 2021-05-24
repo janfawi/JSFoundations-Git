318ead9e3f462539ad1762de51b1b7d9d2ef9dcb
janfawi <jenfawi@gmail.com>
2021-05-24 17:04:46 +0300

    first project

---
+++ story.js
@@ -0,0 +1,5 @@
+let NAME = "mohammad" 
+let AGE = 22 
+let HOBBY = "swimming"
+
+console.log(`Mr./Mrs.  ${NAME}  , in 33 years, you will be ${AGE+33} years old. By then you'll have decades of  ${HOBBY} practice!` )
