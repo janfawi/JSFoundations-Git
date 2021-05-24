b4e48bf3f18528939c7820be79c613f773558939
janfawi <jenfawi@gmail.com>
2021-05-24 19:03:33 +0300

    first calculator

---
+++ calculator.js
@@ -0,0 +1,15 @@
+const firstnumber = 3;
+const secondnumber = 5;
+let operator = "/";
+if (operator === "+") {
+  console.log(firstnumber + secondnumber);
+}
+if (operator === "-") {
+  console.log(firstnumber - secondnumber);
+}
+if (operator === "*") {
+  console.log(firstnumber * secondnumber);
+}
+if (operator === "/") {
+  console.log(firstnumber / secondnumber);
+}
