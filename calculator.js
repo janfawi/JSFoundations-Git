86517c402f1f63cd052c47f31c8364f458936ca3
janfawi <jenfawi@gmail.com>
2022-02-28 00:42:10 +0300

    uodated

--- calculator.js
+++ calculator.js
@@ -1,15 +1,13 @@
 const firstnumber = 3;
 const secondnumber = 5;
 let operator = "/";
-if (operator === "+") {
-  console.log(firstnumber + secondnumber);
-}
-if (operator === "-") {
-  console.log(firstnumber - secondnumber);
-}
-if (operator === "*") {
-  console.log(firstnumber * secondnumber);
-}
-if (operator === "/") {
-  console.log(firstnumber / secondnumber);
-}
+
+operator === "+"
+  ? console.log(firstnumber + secondnumber)
+  : operator === "-"
+  ? console.log(firstnumber - secondnumber)
+  : operator === "*"
+  ? console.log(firstnumber * secondnumber)
+  : operator === "/"
+  ? console.log(firstnumber / secondnumber)
+  : console.log();
--- story.js
+++ story.js
@@ -1,5 +1,9 @@
-let NAME = "mohammad" 
-let AGE = 22 
-let HOBBY = "swimming"
+let NAME = "mohammad";
+let AGE = 32;
+let HOBBY = "swimming";
 
-console.log(`Mr./Mrs.  ${NAME}  , in 33 years, you will be ${AGE+33} years old. By then you'll have decades of  ${HOBBY} practice!` )
+console.log(
+  `Mr./Mrs.  ${NAME}  , in 33 years, you will be ${
+    AGE + 33
+  } years old. By then you'll have decades of  ${HOBBY} practice!`
+);
